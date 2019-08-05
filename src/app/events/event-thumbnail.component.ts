da2588427abd01c0248dce6a3add790ef6fcf477
Steven Lawson <unluckygamblermusic@gmail.com>
2019-08-05 17:59:38 -0600

    using ngSwitch to show various start times with extra info

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,28 +1,40 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'event-thumbnail',
+  selector: "event-thumbnail",
   template: `
     <div class="well hoverwell thumbnail">
       <h2>{{ event?.name }}</h2>
       <div>Date: {{ event?.date }}</div>
-      <div>Time: {{ event?.time }}</div>
+      <div [ngSwitch]="event?.time">Time: {{ event?.time }}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+      </div>
       <div>Price: \${{ event?.price }}</div>
-      <div [hidden]="!event?.location">
+      <div *ngIf="event?.location">
         <span>Location: {{ event?.location?.address }}</span>
-        <span class="pad-left">{{ event?.location?.city }}, {{ event.location?.country }}</span>
+        <span class="pad-left"
+          >{{ event?.location?.city }}, {{ event.location?.country }}</span
+        >
       </div>
-      <div [hidden]="!event?.onlineUrl">
-        Online Url: {{ event?.onlineUrl}}
-      </div>
+      <div *ngIf="event?.onlineUrl">Online Url: {{ event?.onlineUrl }}</div>
     </div>
   `,
-  styles: [`
-    .pad-left {  margin-left: 10px; }
-    .well div { color: #bbb };
-    .thumbnail { min-height: 210px };
-  `]
+  styles: [
+    `
+      .pad-left {
+        margin-left: 10px;
+      }
+      .well div {
+        color: #bbb;
+      }
+      .thumbnail {
+        min-height: 210px;
+      }
+    `
+  ]
 })
 export class EventThumbnailComponent {
-  @Input() event:any
+  @Input() event: any;
 }
